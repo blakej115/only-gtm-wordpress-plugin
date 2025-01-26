6e66ddb5d45723af17d3368385df954b6e91025a
blakej115 <blakej115@gmail.com>
2025-01-26 00:45:14 -0500

    Fixing issues reported by plugin check

--- gtm.php
+++ gtm.php
@@ -49,7 +49,7 @@ class OnlyGTM {
 		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','<?= $this->gtm_id; ?>');</script>
+		})(window,document,'script','dataLayer','<?php echo esc_html($this->gtm_id); ?>');</script>
 		<!-- End Google Tag Manager -->
 	<?php }
 
@@ -59,7 +59,7 @@ class OnlyGTM {
 	function add_noscript()
 	{ ?>
 		<!-- Google Tag Manager (noscript) -->
-		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= $this->gtm_id; ?>"
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo esc_html($this->gtm_id); ?>"
 		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 		<!-- End Google Tag Manager (noscript) -->
 	<?php }
--- readme.txt
+++ readme.txt
@@ -1,6 +1,8 @@
 === Only GTM ===
 Contributors: blakej115
-Tags: only gtm, only, gtm, gtm id, id, google tag manager, simple, lightweight
+Tags: gtm, google tag manager, id, only, simple
+Tested up to: 6.7
+Stable tag: 1.0.0
 License: GPLv2 or later
 
 A simple plugin to add Google Tag Manager to your site.
